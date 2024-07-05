2d309dac4e90f9e599797c78b100ea0463dc460a
Philipp Piwo <github@piwo.space>
2024-07-05 09:19:36 +0200

    fix: only open web3 modal once reconnect finished

--- js/ui/src/App.svelte
+++ js/ui/src/App.svelte
@@ -38,11 +38,10 @@
 		featuredWalletIds: [],
 	});
 
-	reconnect(config)
-
 	let client_metadata = {};
 	onMount(async () => {
 		try {
+			reconnect(config).then(() => web3modal.open())
 			client_metadata = fetch(`${window.location.origin}/client/${client_id}`).then((response) => response.json());
 		} catch (e) {
 			console.error(e);
@@ -72,8 +71,9 @@
 				});
 
 				const preparedMessage = msgToSign.prepareMessage();
-				
-				await new Promise((resolve) => setTimeout(resolve, 1000));
+
+				// no clue why this was needed
+				// await new Promise((resolve) => setTimeout(resolve, 1000));
 				
 				const signature = await signMessage(config,{
 					message: preparedMessage,
@@ -105,7 +105,6 @@
 		oidc_nonce_param = `&oidc_nonce=${oidc_nonce}`;
 	}
 
-	web3modal.open();
 </script>
 
 <div
